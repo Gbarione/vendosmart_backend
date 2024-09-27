96c973a981e0fa09d9af2aa393d7552fd81cafc2
Gustavo Barione <gustavobarione15@gmail.com>
2024-09-26 23:53:20 -0300

    chore: Create an external AppDataSource from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,27 +4,19 @@ import { AppService } from "./app.service";
 import { VendorModule } from "./vendor/vendor.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
+import { LocationModule } from "./location/location.module";
+import { AppDataSource } from "typeorm.config";
 
 @Module({
 	imports: [
 		TypeOrmModule.forRootAsync({
 			useFactory: async () => {
-				const dataSource = new DataSource({
-					type: process.env.DB_TYPE as "mysql" | "mariadb",
-					host: process.env.DB_HOST,
-					port: parseInt(process.env.DB_PORT || "3306", 10),
-					username: process.env.DB_USERNAME,
-					password: process.env.DB_PASSWORD ?? "",
-					database: process.env.DB_NAME,
-					entities: [],
-					synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
-				});
-
-				await dataSource.initialize();
-				return dataSource.options;
+				await AppDataSource.initialize();
+				return AppDataSource.options;
 			},
 		}),
 		VendorModule,
+		LocationModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
