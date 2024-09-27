c59e5ea2bfc1b2709250b39fbd28af3a95836a5a
Gustavo Barione <gustavobarione15@gmail.com>
2024-09-27 00:25:31 -0300

    fix: Service Entity

--- src/service/service.entity.ts
+++ src/service/service.entity.ts
@@ -1,14 +1,19 @@
 import { BaseEntity } from "../_core/entities/base_entity";
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { Vendor } from "../vendor/vendor.entity";
+import { Category } from "../category/category.entity";
 
 @Entity()
 export class Service extends BaseEntity<Service> {
-	@Column({ type: "varchar", length: 255, nullable: false })
-	name: string;
-
 	@ManyToOne(() => Vendor)
 	@JoinColumn({ name: "vendorId" })
 	vendor: Vendor;
+
+	@ManyToOne(() => Category)
+	@JoinColumn({ name: "categoryId" })
+	category: Category;
+
+	@Column({ type: "boolean", default: false })
+	compliant: boolean;
 }
 
