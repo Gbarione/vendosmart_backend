0348e676ae87efa6589300667c6c521faa30f211
Gustavo Barione <gustavobarione15@gmail.com>
2024-09-28 20:14:11 -0300

    feat: Add http interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ dotenv.config();
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "./_core/pipes/validation.pipe";
+import { HttpExceptionFilter } from "./_core/interceptors/http.interceptor";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalFilters(new HttpExceptionFilter());
 	await app.listen(process.env.PORT || 8090);
 }
 bootstrap();
+
